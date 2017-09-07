111ef0bdcfffb08ce273894804121c1b0953b813
iamdlynm <dbum.35@hotmail.com>
2017-09-07 08:35:28 -0600

    first commit

---
+++ Scripts/Coffee.js
@@ -0,0 +1,15 @@
+var b = document.getElementById('myAddToCartButton');
+
+b.onclick = function () {
+    alert('Your coffe was added to your cart')
+}
+
+
+
+
+
+////COMMENTS///////
+var testButton = document.getElementById('testButton');
+testButton.onclick = function () {
+    alert("Coffee added to your cart");
+}
